7c30d7f0925ba8e2763fb769230b392b1d847ef9
Jose <jlhm2077@gmail.com>
2019-10-24 13:26:37 +0200

    filtro componentes

--- app/app.module.js
+++ app/app.module.js
@@ -10,74 +10,8 @@ angular.module('phonecatApp', [
   'phoneComparador',
   'phoneNuevo',
   'phoneCarrito',
-  'app.controller.app',
-  'app.filter.range'
+  'phoneFilter'
 ]);
 
 
-angular.module('app.filter.range',[])
-  .filter('rangeFilter', function () {
-  return function (items, attr, min, max) {
-      var range = [],
-          min=parseFloat(min),
-          max=parseFloat(max);
-      for (var i=0, l=items.length; i<l; ++i){
-          var item = items[i];
-          if(item[attr]<=max && item[attr]>=min){
-              range.push(item);
-          }
-      }
-      return range;
-  };
-});
-
-
-
-
-angular.module('app.controller.app',[])
-  .controller('AppCtrl',[
-  '$scope',
-  function($scope){
-      $scope.model = {}
-      $scope.model.products = [
-          { name: 'A', price: 100  },
-          { name: 'B', price: 500  },
-          { name: 'C', price: 120  },
-          { name: 'D', price: 100  },
-          { name: 'E', price: 1.99 },
-          { name: 'F', price: 710  },
-          { name: 'G', price: 123  },
-          { name: 'H', price: 630  },
-          { name: 'I', price: 458  },
-          { name: 'J', price: 820  }            
-      ];
-
-      $scope.ui = {
-          min: 0,
-          max: 250,
-          sort: 'name'
-      };      
-
-          // Replaced by directive below 
-          // $scope.$watch('ui.min',function(nVal,oVal){
-          //    $scope.ui.min = parseInt(nVal);
-          // })
-
-          // $scope.$watch('ui.max',function(nVal,oVal){
-          //    $scope.ui.max = parseInt(nVal);
-          // })
-      }
-  ])
-
-
-  //  directive to parse input as float (not string)
-  .directive('input', function() {
-      return {
-          restrict: 'E',
-          require: '?ngModel',
-          link: function(scope, element, attrs, ngModel) {
-              if('numeric' in attrs) { ngModel.$parsers.push(parseFloat); }
-          }
-      };
-  });
 
---
+++ app/phone-filter/phone-filter.module.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// Define the `phoneFilter` module
+angular.module('phoneFilter', ['core.phone']);
