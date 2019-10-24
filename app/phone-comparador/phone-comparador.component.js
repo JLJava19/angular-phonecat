8417562df161c236105f2855f511d36552cc5374
Jose <jlhm2077@gmail.com>
2019-10-24 09:13:06 +0200

    comparador detalle

--- app/phone-comparador/phone-comparador.component.js
+++ app/phone-comparador/phone-comparador.component.js
@@ -6,30 +6,57 @@ angular.
   component('phoneComparador', {
     templateUrl: 'phone-comparador/phone-comparador.template.html',
     controller: ['Phone',
-      function PhoneComparadorController(Phone) {
+      function PhoneComparadorController(Phone) {        
+
+        console.trace('PhoneComparadorController');
+
         var self = this;
+
         self.phones = Phone.query();
         self.phone1 = {};
         self.phone2 = {};
-        
-
         self.orderProp = 'age';
 
-        self.seleccionar = function(phone){
-          console.warn('Seleccionar');
-          if(!self.phone1 && !self.phone2){
-            self.phone1 = phone;
-          }else if (self.phone1 && !self.phone2){
-            self.phone2 = phone;
-          }else{
-            self.phone1 = self.phone2;
-            self.phone2 = phone;
-          }
+       
 
+        this.selecionar = function(phone){
+          console.trace('seleccionado movil');
+          self.phone2 = self.phone1;
+          self.phone1 = phone;
         }
+        // selecionar
 
-        
-        
+      }
+    ]
+  });
+
+  /**
+   * Detalle del Comparador
+   */
+  angular.
+  module('phoneComparador').
+  component('phoneComparadorDetalle', {
+    templateUrl: 'phone-comparador/phone-comparador-detalle.template.html',
+    bindings:{
+      mostrar : '=',
+      comparar : '='
+    },
+    controller: [
+      function PhoneComparadorDetalleController() {        
+
+        console.trace('PhoneComparadorDetalleController');
+        var self = this;
+
+        //self.p1 = this.phone1;
+
+        /*
+        this.$onChanges = function (changes) {
+          console.debug('cambia valor %o', changes);
+          self.p1 = changes.phone1.currentValue;
+        };
+        */
+
+
 
       }
     ]
